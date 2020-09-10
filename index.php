f5dcf6a246c4ff85b16897152a6e893bc1701275
jg-digital-media <mail@jonniegrieve.co.uk>
2020-09-10 10:39:39 +0100

    php fragments for page titles

--- index.php
+++ index.php
@@ -7,13 +7,13 @@
     $meta_image = "";
 
     $page_url = "";
-    $page_title = "";
+    $page_title = "Page Home";
 
 ?>
 
 <?php require "inc/header.php";  ?>
 
-<h2>L2 Subtitle</h2>
+<h2><?php echo $page_title;  ?></h2>
 
 <div>
     <p>content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, corrupti inventore. Illum in molestiae, adipisci quae vero nam quasi amet pariatur expedita deserunt asperiores sapiente accusantium quibusdam, voluptatum exercitationem iusto.</p>
