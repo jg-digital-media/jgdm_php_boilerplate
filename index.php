d4d7c2c49718c15c8ac4b30bdb548c449b15f221
jg-digital-media <mail@jonniegrieve.co.uk>
2020-09-11 07:13:13 +0100

    introduce sub content block

--- index.php
+++ index.php
@@ -16,8 +16,19 @@
 
 <h2><?php echo $page_title;  ?></h2>
 
-<div>
+<div class="main_content">
+
+    <h3>Content.</h3> 
+    
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, corrupti inventore. Illum in molestiae, adipisci quae vero nam quasi amet pariatur expedita deserunt asperiores sapiente accusantium quibusdam, voluptatum exercitationem iusto.</p>
+
     <p>content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, corrupti inventore. Illum in molestiae, adipisci quae vero nam quasi amet pariatur expedita deserunt asperiores sapiente accusantium quibusdam, voluptatum exercitationem iusto.</p>
+
+    <p>content. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, corrupti inventore. Illum in molestiae, adipisci quae vero nam quasi amet pariatur expedita deserunt asperiores sapiente accusantium quibusdam, voluptatum exercitationem iusto.</p>
+</div>
+
+<div class="sub_content">
+    <img src="img/placeholder.png" alt="" id="" />
 </div>
 
 <?php require "inc/footer.php"; ?>
