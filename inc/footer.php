4d2ffe3b417060d182713d2068f67550b76e46e3
jg-digital-media <mail@jonniegrieve.co.uk>
2022-06-21 13:16:04 +0100

    echo curent year

--- inc/footer.php
+++ inc/footer.php
@@ -3,7 +3,7 @@
     </main>
 
     <footer>
-        <p>&copy; 2020 Created by Jonnie Grieve Digital Media <a href="https://www.jonniegrieve.co.uk">https://www.jonniegrieve.co.uk</a>
+        <p>&copy; <?php echo date('Y') ?> Created by Jonnie Grieve Digital Media <a href="https://www.jonniegrieve.co.uk">https://www.jonniegrieve.co.uk</a>
     </footer>
 
     <script type="text/javascript" src="scripts/app.js"></script>
